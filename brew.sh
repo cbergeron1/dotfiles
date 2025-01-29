4912c8d0faa2ea674b167b41e29127f53f707ff7
Carlton Bergeron <cjberger1993@outlook.com>
2025-01-28 22:06:29 -0500

    Adding updated brew script with my useful formula

--- brew.sh
+++ brew.sh
@@ -1,6 +1,43 @@
 #!/usr/bin/env bash
 
-# Install command-line tools using Homebrew.
+BREW_FORMULA=(
+	git
+	gh
+	tfenv
+	coreutils
+	moreutils
+	wget
+	awscli
+	automake
+	cmake
+	jq
+	docker
+	pidof
+	watch
+	httpie
+	xz
+	ripgrep
+	mitmproxy
+	gnu-sed
+	kafka
+	tmux
+	jesseduffield/lazygit/lazygit
+	gdu
+	bottom
+	neovim
+	curl
+	m-cli	
+)
+
+FAILED_FORMULA=()
+
+# Check if homebrew is installed already
+if command -v brew &>/dev/null; then
+	echo "Homebrew is installed already, time to update."
+else
+	echo "Installing Homebrew!"
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
 
 # Make sure we’re using the latest Homebrew.
 brew update
@@ -11,91 +48,34 @@ brew upgrade
 # Save Homebrew’s installed location.
 BREW_PREFIX=$(brew --prefix)
 
-# Install GNU core utilities (those that come with macOS are outdated).
-# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-brew install coreutils
+for formula in "${BREW_FORMULA[@]}"; do
+	if brew list "$formula" &>/dev/null; then
+		echo "$formula is already installed."
+	else
+		echo "$formula is not installed. Installing now..."
+		brew install "$formula"
+		
+		# Check install was successful
+		if brew list "$formula" &>/dev/null; then
+			echo "$formula installed successfully."
+		else
+			echo "Failed to install $formula."
+			FAILED_FORMULA+=("$formula")
+		fi
+	fi
+done
+
+PATH=$PATH:$(brew --prefix coreutils)/libexec/gnubin
 ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
-
-# Install some other useful utilities like `sponge`.
-brew install moreutils
-# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
-brew install findutils
-# Install GNU `sed`, overwriting the built-in `sed`.
-brew install gnu-sed --with-default-names
-# Install a modern version of Bash.
-brew install bash
-brew install bash-completion2
-
-# Switch to using brew-installed bash as default shell
-if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
-  echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
-  chsh -s "${BREW_PREFIX}/bin/bash";
-fi;
-
-# Install `wget` with IRI support.
-brew install wget --with-iri
-
-# Install GnuPG to enable PGP-signing commits.
-brew install gnupg
-
-# Install more recent versions of some macOS tools.
-brew install vim --with-override-system-vi
-brew install grep
-brew install openssh
-brew install screen
-brew install php
-brew install gmp
-
-# Install font tools.
-brew tap bramstein/webfonttools
-brew install sfnt2woff
-brew install sfnt2woff-zopfli
-brew install woff2
-
-# Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install aircrack-ng
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
-brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
-brew install xz
-
-# Install other useful binaries.
-brew install ack
-#brew install exiv2
-brew install git
-brew install git-lfs
-brew install gs
-brew install imagemagick --with-webp
-brew install lua
-brew install lynx
-brew install p7zip
-brew install pigz
-brew install pv
-brew install rename
-brew install rlwrap
-brew install ssh-copy-id
-brew install tree
-brew install vbindiff
-brew install zopfli
 
 # Remove outdated versions from the cellar.
 brew cleanup
+
+if [ ${#FAILED_FORMULA[@]} -eq 0 ]; then
+	echo "All specified formula are now installed."
+else
+	echo "The following formula failed to install:"
+	for failed_formula in "${FAILED_FORMULA[@]}"; do
+		echo "- $failed_formula"
+	done
+fi
